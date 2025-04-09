86646ebb3ba7ea3d2bd0b647f49c368cb29b524a
TobiaszGans <tobgan@icloud.com>
2025-04-09 15:52:21 +0200

    Added the option to request  a list of all library User groups. Added the option to change the auto delete settings of these groups

--- main.py
+++ main.py
@@ -264,6 +264,151 @@ def getBookingHistory():
     print('Saving to Output.csv')
     resourceDF.to_csv('Output.csv')
 
+def getLibraries():
+    os.system('cls')
+    print('Welcome to get Libraries.')
+    print('Authenticating...')
+    bearer = getBearer()
+    command = 'users/usergroups?limit=20&start=0&sort=Name&includeSubnetworksUserGroups=false'
+    fullUrl = baseUrl + command
+    headers = {
+            "authorization": bearer}
+    response =  json.loads(requests.get(url=fullUrl, headers=headers, verify='cert.pem').text)
+    size = response["size"]
+    limit = int(response["limit"])
+    if size > limit:
+        paging = True
+        fullPages = size // limit
+        reminder = size % limit
+        if reminder == 0:
+            pages = fullPages
+        else:
+            pages = fullPages + 1
+        print(f'size: {size} entries ({pages} pages)')
+    else:
+        paging = False
+    if paging:
+        start = 0
+        jsonList = []
+        for i in tqdm(range(0, pages), desc='Downloading user groups'):
+            command = f'users/usergroups?limit=20&start={start}&sort=Name&includeSubnetworksUserGroups=false'
+            response = requests.get(url= baseUrl + command, headers=headers, verify='cert.pem')
+            responseJson = json.loads(response.text)["items"]
+            jsonList.extend(responseJson)
+            start = start + limit
+        fullJson = jsonList
+    else:
+        fullJson = json.loads(response.text)["items"]
+    userGroupsDf = pd.json_normalize(fullJson)
+    librariesDf = userGroupsDf.loc[userGroupsDf['isLibraryEnabled'] == True].reset_index()[['id', 'name']]
+    print('Saving groups with libraries to LibraryGroups.csv')
+    librariesDf.to_csv('LibraryGroups.csv')
+    
+def changeAutoDeleteSettings():
+    os.system('cls')
+    print('Welcome to Change auto-delete type.')
+    filePathValid = False
+    while not filePathValid:
+        filePath = input('Please enter the file path to your .csv file: ')
+        extention = filePath[-4:]
+        if extention == ".csv":
+            filePathValid = os.path.isfile(filePath)
+            if not filePathValid:
+                print("Could not find the file.")
+        else:
+            print("File must be a .csv file")
+    libraryGroupsDf = pd.read_csv(filePath, index_col=0, encoding='UTF-8')
+    deleteMode =[
+    '1. Auto Delete all Content',
+    '2. Auto Delete unalocated content only',
+    '3. Disable autodelete'
+    ]
+    for item in deleteMode:
+        print(item)
+    selection = input("\nPlease type script number: ")
+    selectionVerify = False
+    while not selectionVerify:
+        try:
+            int(selection)
+            if int(selection) <= len(deleteMode) and int(selection) >= 1:
+                selectionVerify = True
+            else:
+                selection = input("Incorrect number. Please type option number: ")
+        except:
+            selection = input("Incorrect input type. Please type option number: ")
+
+    if selection == '1':
+        deleteType = 'Any'
+        expiry = True
+        duration = 365
+    elif selection == '2':
+        deleteType = 'Unallocated'
+        expiry = True
+        duration =365
+    elif selection == '3':
+        duration =0
+        deleteType = 'Undefined'
+        expiry = False
+
+    idList = libraryGroupsDf['id'].tolist()
+    if len(idList) == 1:
+        message = str(len(idList)) + ' library.'
+    else:
+        message  = str(len(idList)) + ' libraries.'
+    print(f'You are about to modify settings for {message}')
+    consentValid = False
+    while not consentValid:
+        consent = input('Are you sure you want to conitnue? (y/n): ')
+        if consent == 'y':
+            consentValid = True
+        elif consent == 'n':
+            consentValid = True
+            print('Aborting.')
+            quit()
+        else:
+            consentValid = False
+
+    payload = {
+    "isLibraryEnabled": True,
+    "libraryConfiguration": {
+        "contentExpiryDuration": duration,
+        "contentExpiryType": deleteType,
+        "isContentExpiryEnabled": expiry
+        }
+    }
+
+    print('Authenticating.')
+    bearer = getBearer()
+    headers = {
+            "authorization": bearer}
+    errors = []
+    for group in tqdm(idList):
+        command = f'users/usergroups/{group}'
+        fullUrl = baseUrl + command
+        response = requests.patch(url=fullUrl, json= payload, headers=headers, verify='cert.pem')
+        if response.status_code != 200:
+            responseError = True
+            #patName = libraryGroupsDf.loc[libraryGroupsDf['id'] == group, 'name'].item
+            errorMessage = f'''
+Error on patch request for group ID: {group} ({libraryGroupsDf.loc[libraryGroupsDf['id'] == group, 'name'].item()})
+Error Code: {response.status_code}
+Error Message: {response.text}
+                '''
+            errors.append(errorMessage)
+    if responseError:
+        print(f'There were {len(errors)} erros during the API call.')
+        saveErrorsValid = False
+        while not saveErrorsValid:
+            saveErrors = input('Do you want to save the responses? (y/n): ')
+            if saveErrors == 'y'or saveErrors == 'n':
+                saveErrorsValid = True
+        if saveErrors == 'y':
+            with open('ErrorDump.txt', 'a', encoding='UTF-8') as file:
+                for log in errors:
+                    file.write(log)
+                print('Saved Errors to ErrorDump.txt')
+
+
 
 #------------ Global Variables ------------
 
@@ -279,7 +424,9 @@ def selectScript():
     print('Welcome to the Appspace API interaction tool. Please select required script.')
     menuTxt =[
     '1. Get Channel size',
-    '2. Get Booking History'
+    '2. Get Booking History',
+    '3. Get libraries',
+    '4. Change auto-delete type'
     ]
     for item in menuTxt:
         print(item)
@@ -306,7 +453,11 @@ def main():
     #Add selections when expanding script
     if scriptSelection == 1:
         getChannelSize()
-    elif scriptSelection ==2:
+    elif scriptSelection == 2:
         getBookingHistory()
+    elif scriptSelection == 3:
+        getLibraries()
+    elif scriptSelection == 4:
+        changeAutoDeleteSettings()
     os.remove('./cert.pem')
 main()
